82da6816aaf2caad87741abe10d674d106e02b6e
Anton Klenitskiy <klenitsky.a@insitech.ru>
2022-06-03 18:04:16 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/antklen/diameter-clustering",
-    packages=['diameter_clustering'],
+    packages=['diameter_clustering', 'diameter_clustering.approx'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
